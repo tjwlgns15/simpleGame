fa30d87f02c57d1dae6cfa1abbbea89f4ec581a4
tjwlgns15 <tjwlgns35@naver.com>
2024-07-18 11:43:49 +0900

    first commit

---
+++ src/main/java/com/jihun/simpleGame/SimpleGameApplication.java
@@ -0,0 +1,13 @@
+package com.jihun.simpleGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihun/simpleGame/SimpleGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihun.simpleGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
